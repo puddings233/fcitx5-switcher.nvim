75f30724ffc4436bbd49bd55c5d5c46c32c3561f
puddings233 <puddings233@outlook.com>
2025-11-11 23:14:00 +0800

    feat: fcitx5 start detection.

--- plugin/fcitx5-switcher.lua
+++ plugin/fcitx5-switcher.lua
@@ -1,1 +1,11 @@
-require("fcitx5-switcher-main")
+local function check_fcitx5_available()
+	if os.execute("fcitx5-remote --check > /dev/null") == 0 then
+		return true
+	else
+		return false
+	end
+end
+
+if check_fcitx5_available() then
+	require("fcitx5-switcher-main")
+end
